668600c8ec5d4286ee9a08721559ba99c8e79b90
andreparames <andre.ppereira@parceiros.nos.pt>
2020-04-15 15:23:45 +0100

    Pass request along when path is 404
    When the resolver returns a 404, this middleware was raising the
    exception instead of passing the request on to other middlewares.
    This broke the automatic redirect that appends a trailing slash.

--- django_simple_ip_restrict/middleware.py
+++ django_simple_ip_restrict/middleware.py
@@ -28,15 +28,20 @@ def ip_filter(get_response):
             resolver = get_resolver(urlconf)
         else:
             resolver = get_resolver()
-        resolver_match = resolver.resolve(request.path_info)
-
-        if not protected_namespaces.isdisjoint(resolver_match.namespaces):
-            ip_address = IPAddress(request.META["HTTP_X_FORWARDED_FOR"])
-            if not any(ip_address in subnet for subnet in whitelist):
-                logger.warn(
-                    "IP %s tried to access protected url %s", ip_address, request.path
-                )
-                return HttpResponseForbidden()
+        try:
+            resolver_match = resolver.resolve(request.path_info)
+        except exceptions.Resolver404:
+            pass
+        else:
+            if not protected_namespaces.isdisjoint(resolver_match.namespaces):
+                ip_address = IPAddress(request.META["HTTP_X_FORWARDED_FOR"])
+                if not any(ip_address in subnet for subnet in whitelist):
+                    logger.warn(
+                        "IP %s tried to access protected url %s",
+                        ip_address,
+                        request.path,
+                    )
+                    return HttpResponseForbidden()
 
         response = get_response(request)
         return response
