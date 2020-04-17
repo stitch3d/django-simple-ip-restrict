687ee39106ec70b2921d8a9e31ed4fdc6f6df198
andreparames <andre.ppereira@parceiros.nos.pt>
2020-04-17 09:59:56 +0100

    Rename test

--- tests/test_firewall.py
+++ tests/test_firewall.py
@@ -6,6 +6,6 @@ class TestFirewall(TestCase):
         response = self.client.get("/admin/", HTTP_X_FORWARDED_FOR="83.4.12.1")
         self.assertEqual(response.status_code, 403)
 
-    def test_firewall(self):
+    def test_multiple_ips(self):
         response = self.client.get("/admin/", HTTP_X_FORWARDED_FOR="83.4.12.1, 10.2.1.1")
         self.assertEqual(response.status_code, 403)
